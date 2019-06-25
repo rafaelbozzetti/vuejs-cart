717900846b8e59b4ffdeb0568172ce97631b4171
Rafael Pereira Bozzetti <rafaelbozzetti@gmail.com>
2019-06-25 13:25:58 -0300

    Event Handling, Class and Style Binding

--- main.js
+++ main.js
@@ -7,11 +7,31 @@ var app = new Vue({
     image: './assets/camiseta.png',
     inStock: true,
     details: ["80% algod", "20% poliester"],
+    variants: [
+      {
+        "variantId": 2231,
+        "variantColor": 'white',
+        "variantImage": './assets/camiseta.png'        
+      },    
+      {
+        "variantId": 2234,
+        "variantColor": 'green',
+        "variantImage": './assets/camiseta-verde.png'        
+      },
+      {
+        "variantId": 2235,
+        "variantColor": 'blue',
+        "variantImage": './assets/camiseta-azul.png'
+      }
+    ],
     cart: 0
   },
   methods: {
     addToCart: function() {
       this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
     }
   }
 });
